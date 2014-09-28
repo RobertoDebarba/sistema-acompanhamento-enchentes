af8803f47c929dde78cd18f91182685bb380831c
feenux <luancarlospurim@gmail.com>
2014-09-28 15:13:27 -0300

    Termino do codigo do arduino

--- ARDUINO/ArduinoServerSocket/src/Leitor_Arduino___Socket/Leitor_Arduino___Socket.ino
+++ ARDUINO/ArduinoServerSocket/src/Leitor_Arduino___Socket/Leitor_Arduino___Socket.ino
@@ -1,5 +1,10 @@
+
 #include <SPI.h>
 #include <Ethernet.h>
+#define triggerPin 3
+#define echoPin 2
+//laranja echo 8
+//vermelho trigger 6
 
 byte mac[] = { 
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 }; //Endereço MAC do Arduino
@@ -9,159 +14,119 @@ IPAddress subnet(255, 255, 0, 0);//Endereço SubnetMask(Opcional)
 
 EthernetServer server(5661); //Seta a porta para o SocketServer
 
-int pinPotenciometer = 0;
-int pinRedLed = 2;
-int pinYellowLed = 3;
-int pinGreenLed = 4;
-int pinBlueLed = 5;
+int pino_d = A5;
+int pinLed = 4;
 
-byte serverStatus;
-const byte SERVER_UP = 1;
-const byte SERVER_CONNECTING = 2;
-const byte SERVER_DOWN = 3;
-const byte SENDING_DATA = 4;
 
 
 
 void setup() {
   Serial.begin(9600);
-  
-  pinMode(pinRedLed, OUTPUT); // Seta a porta do led vermelho como saída
-  pinMode(pinYellowLed, OUTPUT); // Seta a porta do led amarelo como saída
-  pinMode(pinGreenLed, OUTPUT); // Seta a porta do led verde como saída
-  pinMode(pinBlueLed, OUTPUT); // Seta a porta do led vermelho como saída
-  
+
+  pinMode(pino_d, INPUT);//pino sensor de chuva
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(pinLed, OUTPUT); // Seta a porta do led vermelho como saída
+ 
+
   //Condicional que inicia a conexao com a internet
   Serial.println("Trying to get an IP address using DHCP");
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    serverStatus = SERVER_CONNECTING;
-    // initialize the ethernet device not using DHCP:
     Ethernet.begin(mac, ip, gateway, subnet);
-  } else {
-    serverStatus = SERVER_UP;
-    statusServerLed();
-  }
-  // print your local IP address:
+  } 
+  // escreve o ip local
   Serial.print("My IP address: ");
   ip = Ethernet.localIP();
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    // print the value of each byte of the IP address:
     Serial.print(ip[thisByte], DEC);
     Serial.print("."); 
   }
   server.begin();
- 
+
 }
 
 void loop(){
- EthernetClient client = server.available();
-  if (client) {
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        if (c == 'getChuva'){
-        }
-        else if (c == 'getElevacao') {
-           client.println(readPotenciometer());
-           receivingSendingMessage(); 
-        }
-        Serial.write(c);     
-      }
+EthernetClient client = server.available();
+ if (client) {   
+    if (client.available()) {      
+      client.println(JSONLeituras());
+      piscaLed();
     }
   }
 }
 
-/**
-  *Faz a leitura do potenciometro
-  *<p>
-  *<b>Exemplo </p>
-  *<code> readPotenciometer(); </code>
-  *<p>
-  *@return retorna o valor de leitura do potenciometro
-  *
-  *@author Luan
-  *@version 1.0
-*/
-int readPotenciometer() { 
-  return analogRead(pinPotenciometer);;
+String leituraChuva (){
+  int valorLeitura = analogRead(pino_d);
+  int valorRetorno;
+  if(valorLeitura>900 && valorLeitura<1023){
+    valorRetorno = 0;//sem chuva
+  } else if (valorLeitura>450 && valorLeitura<900){
+    valorRetorno = 1; //chuva moderada
+  } else if (valorLeitura>0 && valorLeitura<450){
+    valorRetorno = 2;//chuva intensa
+  } 
+  return String(valorRetorno);
 }
-  
-/**
-  *Faz o led acender de acordo com o status do server socket
-  *<p>
-  *<b>Exemplo: </p>
-  *<code> statusServerLed(); </code>
-  *<p>
-  *
-  *@author Luan
-  *@version 1.0
-*/
-void statusServerLed() {
-  switch (serverStatus) {
-    case SERVER_UP:
-      statusServerLedUp(pinGreenLed);
-      statusServerLedDown(pinYellowLed, pinRedLed);
-      break;
-    case SERVER_CONNECTING:
-      statusServerLedUp(pinYellowLed);
-      statusServerLedDown(pinGreenLed, pinRedLed);
-      break;
-    case SERVER_DOWN:
-      statusServerLedUp(pinRedLed);
-      statusServerLedDown(pinGreenLed, pinYellowLed);
-      break;
-  }
-} 
 
-/**
-  *Acende o led de acordo parametro passado; <br>
-  *<p>
-  *<b>Exemplo: </p>
-  *<code> statusServerLedUp(pinGreenLed); </code>
-  *</p>
-  *@param setPin seta o pino do que sera aceso 
-  *
-  *@author Luan
-  *@version 1.0
-*/
-void statusServerLedUp (int setPin) {
-  digitalWrite(setPin, HIGH);
+String JSONLeituras () {
+  String inicio = "{\"nivelChuva\": ";
+  String meio = ", \"nivelRio\": ";
+  String fim = " }";
+  String leituras = inicio+leituraChuva()+meio+readUltrasonicSensor()+fim;
+  return leituras;
 }
 
 /**
-  *Apaga os leds de acordo com o status do server socket; <br>
-  *<p>
-  *<b> Exemplo: </p>
-  *<code> statusServerLedDown(pinYellowLed, pinRedLed); </code>
-  *<p>
-  *@param setPin1 seta o pino do led que sera apagado
-  *@param setPin2 seta o pino do led que sera apagado
-  *
-  *@author Luan
-  *@version 1.0
-*/
-void statusServerLedDown (int setPin1,int setPin2) {
-  digitalWrite(setPin1, LOW);
-  digitalWrite(setPin2, LOW);
-}
-
-
-/**
-  *Faz o led azul piscar quando houver uma requisicao do client socket
-  *<p>
-  *<b> Exemplo: </p>
-  *<code> receivingSendingMessage(); </code>
-  *<p>
-  *
-  *@author Luan
-  *@version 1.0
-*/
-void receivingSendingMessage(){
-  digitalWrite(pinBlueLed, HIGH);
+ *Faz o led azul piscar quando houver uma requisicao do client socket
+ *<p>
+ *<b> Exemplo: </p>
+ *<code> receivingSendingMessage(); </code>
+ *<p>
+ *
+ *@author Luan
+ *@version 1.0
+ */
+void piscaLed(){
+  digitalWrite(pinLed, HIGH);
   delay(300);
-  digitalWrite(pinBlueLed, LOW); 
+  digitalWrite(pinLed, LOW); 
 }
 
+String readUltrasonicSensor(){
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  
+  // O sensor calcula o tempo gasto entre o envio e o recebimento
+  // do sinal e retorna um pulso com esta duração
+  long duration = pulseIn(echoPin, HIGH);
 
+  // Converte o tempo para distancia em centimetros
+  float cm = microsecondsToCentimeters(duration);
+  
+  // Informa a distancia na serial
+  
+  char leitura[100];
+     
+  dtostrf(cm,2,2,leitura);
+  
+ return leitura;
+}
 
+float microsecondsToCentimeters(long microseconds){
+  // Converte o tempo de microsegundos para segundos
+  float seconds = (float) microseconds / 1000000.0;
+  // Com a velocidade do som de 340m/s calcula-se a
+  // distancia percorrida
+  float distance = seconds * 340;
+  // Divide o resultado por dois pois o tempo é calculado
+  // considerando a ida e a volta do sinal  
+  distance = distance / 2;
+  // Converte o resultado em metros para centimetros
+  distance = distance * 100;
+  
+  return distance;
+}
