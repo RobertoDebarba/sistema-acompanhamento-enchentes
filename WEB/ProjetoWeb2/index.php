fb04eb29ff589fb073c2790a4b58eb8064eacbc6
robertodebarba <roberto.debarba@gmail.com>
2014-10-30 21:26:35 -0200

    Remoção do do diretorio App

--- WEB/ProjetoWeb2/index.php
+++ WEB/ProjetoWeb2/index.php
@@ -1,14 +1,3 @@
 <?php
-	include './Mobile_Detect.php';
-	
-	$detect = new Mobile_Detect();
-	
-	//se for acesso movel
-	if($detect->isMobile()){
-		header('Location: App/index.php');
-	}
-	//se for acesso web desktop
-	else {
-		header('Location: Web/index.php');
-	}	
+	header('Location: Web/index.php');
 ?>
