e25b896bc519f20c05b69154e8be499b45d81c76
RobertoDebarba <roberto.debarba@gmail.com>
2014-11-06 19:35:48 -0200

    Correção do local de arquivos

--- WEB/ProjetoWeb2/Web/footer.php
+++ WEB/ProjetoWeb2/Web/footer.php
@@ -4,7 +4,7 @@
 		<script src="js/geral/ie10-viewport-bug-workaround.js"></script>
 
 		<!-- JQuery -->
-		<script src="js/jquery.min.js"></script>
+		<script src="js/geral/jquery.min.js"></script>
 
 		<!-- Bootstrap -->
 		<script src="bootstrap-3.2.0/js/bootstrap.min.js"></script>	
