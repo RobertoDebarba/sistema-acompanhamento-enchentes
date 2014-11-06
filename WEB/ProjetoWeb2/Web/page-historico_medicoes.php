43d25a15bbe03bcb8bf8e7c645c756442a69c8be
RobertoDebarba <roberto.debarba@gmail.com>
2014-11-06 21:56:17 -0200

    Continuação no desenvolvimento da page-historico_mediçoes

--- WEB/ProjetoWeb2/Web/page-historico_medicoes.php
+++ WEB/ProjetoWeb2/Web/page-historico_medicoes.php
@@ -1,37 +1,72 @@
-<?include "../Comum/php/funcoes.php";  ?><html>
-<head>
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-	<script type="text/javascript">
-		function drawChart() {
-			var data = google.visualization.arrayToDataTable([
-				['Data', 'Nível do Rio', ],
-				<?php
-					$leituras = getLeituras(20, true);
-					
-					foreach ($leituras as $leitura) {
-						echo "['$leitura[0]', $leitura[1]],";
-					}
-				?>
-			]);
-		
-			var options = {
-				title: 'Histórico de Medições',
-			    
-				legend: { position: 'none' },
-				pointSize: 5
-			};
-		
-			var chart = new google.visualization.LineChart(document.getElementById('chartRio'));
-			
-			chart.draw(data, options);
-		}
-		
-		google.load("visualization", "1", {packages:["corechart"]});
-		google.setOnLoadCallback(drawChart);
-	</script>
-</head>
-<body>
-	<div id="chartRio" style="width: 1500px; height: 500px;"></div>
-</body>
-</html>
+<?php
+	include 'header.php';
+	$meta = array();
 
+	$meta['pag_inicial'] = false;
+	$meta['pagina'] = 'Historico de Medições';
+	$meta['descricao'] = 'Historico de Medições';
+	$meta['css'] = array();
+	$meta['menuAtivo'] = 0;
+
+	printHeader($meta);
+
+	$alerta = getEstadoAlerta();
+?>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Data', 'Nível do Rio', ],
+			<?php
+				$leituras = getLeituras(15, true);
+				
+				foreach ($leituras as $leitura) {
+					echo "['$leitura[1]h', $leitura[2]],";
+				}
+			?>
+		]);
+	
+		var options = {
+			title: 'Medição do Nível do Rio',
+			legend: { position: 'none' },
+			pointSize: 5
+		};
+	
+		var chart = new google.visualization.LineChart(document.getElementById('chartRio'));
+		
+		chart.draw(data, options);
+	}
+	
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawChart);
+</script>
+
+<br>
+<br>
+
+<div id="chartRio" style="width: 1300px; height: 500px;"></div>
+
+<table class="table table-striped">
+<thead>
+	<tr>
+		<td><b>Data | Hora</b></td>
+		<td><b>Nível do Rio</b></td>
+		<td><b>Estado da Chuva</b></td>
+	</tr>
+</thead>
+<tbody>
+	<?php 
+		foreach ($leituras as $leitura) {
+			echo "<tr>";
+			echo "<td>$leitura[0] - $leitura[1] h</td>";
+			echo "<td>$leitura[2] m</td>";
+			echo "<td>$leitura[3]</td>";
+			echo "</tr>";
+		}
+	?>
+</tbody>
+
+<?php
+	include 'footer.php';
+	printFooter();
+?>
