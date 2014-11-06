842f832e1a3df60849917160a78daef5df61f59e
jonnymohamed <jonny.suptitz@gmail.com>
2014-11-06 21:58:25 -0200

    versão nova

--- MOVEL/AplicativoMovel/js/funtions.js
+++ MOVEL/AplicativoMovel/js/funtions.js
@@ -1,228 +1,164 @@
 var alertaAtivo;// verificador de timer do alerta ativo ou nao
-    	var timer; // variavel que recebe o timer de alertas
+var timer; // variavel que recebe o timer de alertas
 
-    	var estado; // array com estado de alerta da defesa civil
-    	var leituras;// array com leituras completas de mediçoes
-    	
-    	var dataArray = []; //array com data e nivel do rio
-    	var chuva = []; // array com data e estado da chuva
+var estado; // array com estado de alerta da defesa civil
+var leituras;// array com leituras completas de mediçoes
 
-    	getEstadoAlerta();
-    	getLeituras(12);	
-    
-    	document.addEventListener("deviceready", onDeviceReady, false);
+var dataArray = []; //array com data e nivel do rio
+
+var alturaAlerta;
+
+var altitude;
+var latitude;
+var longitude;
+
+getEstadoAlerta();
+getLeituras(12);	
+
+
+/**
+ * Ativa o modo background e manten um timer de alerta
+ */
+function ativarAlerta(sim){
+	if(sim){
+		timer = setInterval(function(){alerta();}, 1000*5/*15*/);
+		window.plugin.backgroundMode.enable();
+
+		alertaAtivo = true;
+	}
+	else{
+		clearInterval(timer);
 		
-	    function onDeviceReady() {
-	       
-	      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-	    }
+		window.plugin.backgroundMode.disable();
+		 
+		alertaAtivo = false;
+	}
+}
 
 
-	    function lerCfg(){
-	    	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "readme.txt", gotFile, fail);
-	    }
-	
-
-	    function gotFS(fileSystem) {
-	        fileSystem.root.getFile("readme.txt", {
-	        	create: true,
-	        	exclusive: false},
-	        	gotFileEntry, fail);
-	    }
-	
-	    function gotFileEntry(fileEntry) {
-	        fileEntry.createWriter(gotFileWriter, fail);
-	        // fileEntry.file(readAsText, fail);
-	    }
-	
-	    function gotFileWriter(writer) {
-	    	writer.onwrite = function(evt) {
-    			alert("checkpoint 4: write success!")
-    		};
-	           
-	    }
+// chamado quando clicado no botao "Abrir App" do alerta
+function alertaAbrirPrograma() {
+    // do something
+}
 
 
-	    function readAsText(file) {
-	        var reader = new FileReader();
-	        reader.onloadend = function(evt) {
-	           alert(evt.target.result);
-	        };
-	    }
+/**
+ *  o alerta
+ */
+function alerta() {
+	var now = new Date();
+	window.plugin.notification.local.add({
+	    id:      1,
+	    date : now,
+	    title:   'SAEmóvel Alerta',
+	    message: 'Atenção! Água a caminho!',
+	    autoCancel: true
+    });
+}	    	
 
-	    //erro de leitura ou escrita do arquivo
-	    function fail(error) {
-	        alert(error.code);
-	    }
-	    
-	    /**
-	     * Ativa o modo background e manten um timer de alerta
-	     */
-	    function ativarAlerta(sim){
-	    	if(sim){
-	    		timer = setInterval(function(){alerta();}, 1000*5*15);
-	    		window.plugin.backgroundMode.enable();
 
-	    		alertaAtivo = true;
-	    	}
-	    	else{
-	    		clearInterval(timer);
-	    		
-	    		window.plugin.backgroundMode.disable();
-	    		 
-	    		alertaAtivo = false;
-	    	}
-	    }
-	
-	    // chamado quando clicado no botao "Abrir App" do alerta
-        function alertaAbrirPrograma() {
-            // do something
+
+
+/**
+ * obtem as leituras do php no servidor
+ */
+function getLeituras(qtdLeituras){
+	$.ajax({
+        url: "http://localhost/jonathan/Projeto%20Web/final/funcoes.php/?getLeituras=?",
+		data: { 'qtdLeituras' : qtdLeituras},
+	    dataType:'jsonp',
+        crossDomain: true,
+
+        success: function(data){
+        	setleituras(data);
         }
+    });
+}
+
+
+/**
+ * obtem alertas da defesa civil do php no servidor
+ */
+function getEstadoAlerta(){
+ 	$.ajax({
+        url: "http://localhost/jonathan/Projeto%20Web/final/funcoes.php?getEstadoAlerta=?",
+    	dataType:'jsonp',
+        crossDomain: true,
+
+        success: function(data){
+        	setEstado(data);
+		}  
+    });
     
-	    /**
-	     *  o alerta
-	     */
-	    function alerta() {
-	    	navigator.notification.beep(3);
-	        navigator.notification.confirm(
-	           'Atenção! Água a caminho!',  
-	            alertaAbrirPrograma,
-	            'SAEmóvel Alerta',           
-	            ['Abrir App','Ignorar']     
-	        );
-	    }	    	
-		
-		/**
-		 * exibi o menu +
-		 */
-		function exibirMenuPlus(){
-				$("#menuPlus").slideToggle('fast');
-	    }
-	    
-	    /**
-	     * abre a tela passada como parametro
-	     * -apenas nome do arquivo php sem o .php
-	     */
-	    function alterarConteudo(tela){
-	    	if(tela == "home"){	
-	    		$( "#conteudo" ).hide();
-	    		$("#alerta").show();
-	    		$( "#mapaconteiner" ).show();		
-	    	}
-	    	else{
-	    		$( "#conteudo" ).show();
-	    		$( "#mapaconteiner" ).hide();
-		    	$("#menuPlus").hide('fast');
-		    	$("#alerta").hide();
-			    $( "#conteudo" ).load( "./"+tela+".html"); 	
-				
-			}				
-	    }
-		
-		/**
-		 * obtem as leituras do php no servidor
-		 */
-	    function getLeituras(qtdLeituras){
-	    	$.ajax({
-                url: "http://localhost/jonathan/Projeto%20Web/final/funcoes.php/?getLeituras=?",
-       			data: { 'qtdLeituras' : qtdLeituras},
-                dataType:'jsonp',
-                crossDomain: true,
+ }
 
-                success: function(data){
-                	setleituras(data);
-                }
-            });
-	    }
-	    
-	    /**
-		 * obtem alertas da defesa civil do php no servidor
-		 */
-        function getEstadoAlerta(){
-         	$.ajax({
-                url: "http://localhost/jonathan/Projeto%20Web/final/funcoes.php?getEstadoAlerta=?",
-                dataType:'jsonp',
-                crossDomain: true,
 
-                success: function(data){
-                	setEstado(data);
-				}  
-            });
-            
-         }
-		
-         function setEstado(dados){
-         	estado = dados;
-         	alteraBarraAlerta();
-         }
+function setEstado(dados){
+ 	estado = dados;
+ 	alteraBarraAlerta();
+ }
 
-         function setleituras(dados){
-         	leituras = dados;  
-         	alteraBarraAlerta();         	
-         }
+
+function setleituras(dados){
+ 	leituras = dados;  
+ 	alteraBarraAlerta();         	
+ }
+
+
+/**
+ * cria um array com as leituras para o grafico
+ */
+function carregaLeituras(){
+	dataArray = [];
+	dataArray[0] = ['Data', 'Nivel do Rio'];
+
+	var key;
+	for(key in leituras) {
+  		if(leituras.hasOwnProperty(key)) {
+
+	    	var object = leituras[key];				
 		
-		/**
-		 * cria um array com as leituras para o grafico
-		 */
-		function carregaLeituras(){
-			dataArray = [];
-			chuva = [];
-			
-			chuva[0] = ['Data','Chuva'];
-			dataArray[0] = ['Data', 'Nivel do Rio'];
-			
-	    	var key;
-			for(key in leituras) {
-			  	if(leituras.hasOwnProperty(key)) {
-			
-				    var object = leituras[key];
-				    
-				    datacompleta =  object[0] + " "+  object[1];					
-					
-					$('#tabela').append('<tr><td>'+object[0]+" "+object[1] + '</td><td>'+object[2] + ' m</td><td>'+object[3] + '</td></tr>');
-				  	
-				  	if(key <= 6){
-				  		chuva[chuva.length] = [datacompleta, parseInt(object[3])];
-						dataArray[dataArray.length] = [datacompleta, parseInt(object[2])];
-					}
-				}
-			}	
-		}
-			
-		function setDados(data){
-			dados = data;
-		}
-	
-		/**
-		 * altera o design da barra de alerta no rodapé
-		 */
-    	function alteraBarraAlerta(){	   		
-    		medicoes = leituras[1];
-    		
-			if ((estado[0] == 0) & (estado[1] == 0)) {
-				$("#alerta").toggleClass("button-assertive");
-				$("#alerta").toggleClass("button-energized");
-				$("#alerta").html('Nivel do rio: ' + medicoes[2] + 'm | Chuva' + medicoes[3]);
-			} else if ((estado[0] == 1) | ((estado[1] == 1))) {
-				$("#alerta").toggleClass("button-assertive");
-				$("#alerta").toggleClass("button-positive");
-				$("#alerta").html('Nivel do rio: ' + medicoes[2] + 'm | Chuva ' + medicoes[3]);
-			} else if (estado[0] == 2) {
-				$("#alerta").toggleClass("button-energized");
-				$("#alerta").toggleClass("button-positive");
-				$("#alerta").html('Nivel do rio: ' + medicoes[2] + 'm | Chuva ' + medicoes[3]);
+			$('#tabela').append('<tr><td>'+object[0]+" "+object[1] + '</td><td>'+object[2] + ' m</td><td>'+object[3] + '</td></tr>');
+		  	
+		  	if(key <= 7){
+				dataArray[dataArray.length] = [object[1], parseInt(object[2])];
 			}
+		}
+	}	
+}
+
 	
-    	}
+function setDados(data){
+	dados = data;
+}
 
 
+/**
+ * altera o design da barra de alerta no rodapé
+ */
+function alteraBarraAlerta(){	   		
+	medicoes = leituras[1];
+	
+	if ((estado[0] == 0) & (estado[1] == 0)) {
+		$("#alerta").toggleClass("button-assertive");
+		$("#alerta").toggleClass("button-energized");
+		$("#alerta").html('Nivel do rio: ' + medicoes[2] + 'm | Chuva' + medicoes[3]);
+	}
+	else if ((estado[0] == 1) | ((estado[1] == 1))) {
+		$("#alerta").toggleClass("button-assertive");
+		$("#alerta").toggleClass("button-positive");
+		$("#alerta").html('Nivel do rio: ' + medicoes[2] + 'm | Chuva ' + medicoes[3]);
+	}
+	else if (estado[0] == 2) {
+		$("#alerta").toggleClass("button-energized");
+		$("#alerta").toggleClass("button-positive");
+		$("#alerta").html('Nivel do rio: ' + medicoes[2] + 'm | Chuva ' + medicoes[3]);
+	}
+}
 
 
-// onSuccess Callback
-// This method accepts a Position object, which contains the
-// current GPS coordinates
-//
-var alturaAtual = 4;
+var alturaAtual = 4;/**>>>>>QG<<<<<<<*/
+
 function trataEnchentes(data) {
 	for (var oi in data) {
 		var teste = data[oi];
@@ -231,6 +167,7 @@ function trataEnchentes(data) {
 		}
 	}
 }
+
 
 function getEnchentes() {
 	$.ajax({
@@ -244,9 +181,6 @@ function getEnchentes() {
 	});
 }
 
-var altitude;
-var latitude;
-var longitude;
 
 function coordenadas() {
 	alert('Certifique-se que o GPS do dispositivo está ativado para maior precisão');
@@ -255,8 +189,9 @@ function coordenadas() {
 		longitude = position.coords.longitude;
 		geocodificacaoReversa();
 		$("#latitudeLocal").html(latitude);
-		$("#longitudeLocal").html(longitude);
 	
+		$("#longitudeLocal").html(longitude);
+
 	};
 
 	function onError(error) {
@@ -273,9 +208,11 @@ function coordenadas() {
 
 }
 
+
 function geocodificacaoReversa() {
 	var geocoder;
 	geocoder = new google.maps.Geocoder();
+	
 	var latlng = new google.maps.LatLng(latitude, longitude);
 	geocoder.geocode({
 		'latLng' : latlng
@@ -290,6 +227,7 @@ function geocodificacaoReversa() {
 		}
 	});
 }
+
 
 function getAltura() {
 	locations = [];
@@ -308,11 +246,84 @@ function getAltura() {
 				$("#elevacaoLocal").html(results[0].elevation);
 				$("#divbotao").hide();
 				$("#tabela").show();
-			} else {
+				return results[0].elevation;
+			}
+			else{
 				return ('No results found');
 			}
-		} else {
+		}
+		else{
 			alert('Elevation service failed due to: ' + status);
 		}
 	});
+	
+}
+
+/**
+ * adiciona local na pagina Serviços
+ * */
+function addLocal(){
+	coordenadas();
+	//armazenarAltura(getAltura());
+	
+	$("#buscarLocal").html("Alterar Local");
+	
+}
+
+function lerCfg(){
+	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "cfg.txt", gotFile, fail);
+}
+function gotFS(fileSystem) {
+        fileSystem.root.getFile("cfg.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+}       
+
+function gotFileEntry(fileEntry) {
+    fileEntry.createWriter(gotFileWriter, fail);
+}
+
+function gotFileWriter(writer) {
+    // writes a file
+    write_file = function(writer) {
+        var lineCount = 1;
+                  
+        // set the callbacks
+        writer.onwritestart = onFileEvent;
+        writer.onprogress = onFileEvent;
+        writer.onwrite = onFileWrite;
+        writer.onabort = onFileEvent;
+        writer.onerror = onFileError;
+        writer.onwriteend = function(event) {
+            onFileEvent(event);
+            lineCount += 1;
+            if (lineCount <= 3) {
+                // append a new line   
+                writer.write('Line ' + lineCount + '.\r\n');  
+            } 
+            else {
+                alert(writer.fileName + 
+                    ' length=' + writer.length + 
+                    ', position=' + writer.position);
+            }
+        };
+        
+        // append
+            writer.seek(writer.length);
+  
+            // write to file
+        writer.write('Line ' + lineCount + '.\r\n');   
+    };
+}
+        
+
+
+function readAsText(file) {
+    var reader = new FileReader();
+    reader.onloadend = function(evt) {
+       alert(evt.target.result);
+    };
+}
+
+
+ function fail(error) {
+    console.log(error.code);
 }
