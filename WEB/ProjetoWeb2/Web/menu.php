7d43007b99f6f1e9a4148db29fd426481975d129
RobertoDebarba <roberto.debarba@gmail.com>
2014-11-09 19:58:38 -0200

    Adicionado itens galeria e simulador ao menu

--- WEB/ProjetoWeb2/Web/menu.php
+++ WEB/ProjetoWeb2/Web/menu.php
@@ -28,28 +28,18 @@
 							<li<?php if($menuAtivo == 3) {echo 'class="active"';} ?>>
 								<a href="page-previsao_tempo.php">Previsão do Tempo</a>
 							</li>
-							<li <?php if($menuAtivo == 4) {echo 'class="active"';} ?>
-								<a href="#">Link</a>
-							</li>
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
 								<ul class="dropdown-menu" role="menu">
 									<li>
-										<a href="#">Action</a>
+										<a href="page-simulador_rio.php">Simulador de Inundações</a>
 									</li>
 									<li>
-										<a href="#">Another action</a>
-									</li>
-									<li>
-										<a href="#">Something else here</a>
+										<a href="page-galeria.php">Galeria</a>
 									</li>
 									<li class="divider"></li>
 									<li>
 										<a href="#">Separated link</a>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="#">One more separated link</a>
 									</li>
 								</ul>
 							</li>
