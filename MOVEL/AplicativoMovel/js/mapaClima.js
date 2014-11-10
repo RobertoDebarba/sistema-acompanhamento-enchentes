1426b5173318a73a16cc6c65acdd1b95d8651eb7
Feenux <luancarlospurim@gmail.com>
2014-11-10 19:47:26 -0200

    Inserindo mapaClima

--- MOVEL/AplicativoMovel/js/mapaClima.js
+++ MOVEL/AplicativoMovel/js/mapaClima.js
@@ -25,7 +25,7 @@ function initializeWeather() {
 
 	// Create the search box and link it to the UI element.
 	var input = /** @type {HTMLInputElement} */(
-		document.getElementById('pac-input'));
+		document.getElementById('pac-input2'));
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 	var searchBox = new google.maps.places.SearchBox(
@@ -94,99 +94,6 @@ function initializeWeather() {
 		var bounds = map.getBounds();
 		searchBox.setBounds(bounds);
 	});
+	
 }
-
-// [END region_initialization]
-
-// [START region_constructor]
-/** @constructor */
-function USGSOverlay(bounds, image, map) {
-
-	// Initialize all properties.
-	this.bounds_ = bounds;
-	this.image_ = image;
-	this.map_ = map;
-
-	// Define a property to hold the image's div. We'll
-	// actually create this div upon receipt of the onAdd()
-	// method so we'll leave it null for now.
-	this.div_ = null;
-
-	// Explicitly call setMap on this overlay.
-	this.setMap(map);
-}
-
-// [END region_constructor]
-
-// [START region_attachment]
-/**
- * onAdd is called when the map's panes are ready and the overlay has been
- * added to the map.
- */
-USGSOverlay.prototype.onAdd = function() {
-
-	var div = document.createElement('div');
-	div.style.borderStyle = 'none';
-	div.style.borderWidth = '0px';
-	div.style.position = 'absolute';
-
-	// Create the img element and attach it to the div.
-	var img = document.createElement('img');
-	img.src = this.image_;
-	img.style.width = '100%';
-	img.style.height = '100%';
-	img.style.position = 'absolute';
-	div.appendChild(img);
-
-	this.div_ = div;
-
-	// Add the element to the "overlayLayer" pane.
-	var panes = this.getPanes();
-	panes.overlayLayer.appendChild(div);
-};
-// [END region_attachment]
-
-// [START region_drawing]
-USGSOverlay.prototype.draw = function() {
-
-	// We use the south-west and north-east
-	// coordinates of the overlay to peg it to the correct position and size.
-	// To do this, we need to retrieve the projection from the overlay.
-	var overlayProjection = this.getProjection();
-
-	// Retrieve the south-west and north-east coordinates of this overlay
-	// in LatLngs and convert them to pixel coordinates.
-	// We'll use these coordinates to resize the div.
-	var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-	var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-	// Resize the image's div to fit the indicated dimensions.
-	var div = this.div_;
-	div.style.left = sw.x + 'px';
-	div.style.top = ne.y + 'px';
-	div.style.width = (ne.x - sw.x) + 'px';
-	div.style.height = (sw.y - ne.y) + 'px';
-};
-// [END region_drawing]
-
-// [START region_removal]
-// The onRemove() method will be called automatically from the API if
-// we ever set the overlay's map property to 'null'.
-USGSOverlay.prototype.onRemove = function() {
-	this.div_.parentNode.removeChild(this.div_);
-	this.div_ = null;
-};
-// [END region_removal]
-
-$(function() {
-	$('.loadMap').ready(function(e) {
-		e.preventDefault();
-		var map = this.id + '-map';
-
-		if ($('#mapaCLima #' + map).length === 0) {
-			$('<div class="map_style" id="' + map + '">').appendTo('#mapaCLima');
-			initializeWeather();
-		}
-	});
-});
-
+google.maps.event.addDomListener(window, 'load', initializeWeather);
